796ce2c0508db05dc2a50495b1135c53b4f71448
Lucie.Gendron <lucie.gendron.brun@gmail.com>
2023-02-02 12:53:28 +0100

    init repo

---
+++ helloworld.js
@@ -0,0 +1,63 @@
+//let name = "John Doe"
+//const age = 30
+//let isLearning = true
+
+
+//console.log(`je m'appelle ${name} j'ai ${age} ans`)
+
+//let num1 = 10
+//let num2 = 20
+
+//console.log(`${num1}+${num2} = 30`)
+//console.log(`${num1}+${num2}=${num1+num2}`)
+
+//console.log(typeof(num2))
+//let dictionnaire = new Map();
+//  console.log(dictionnaire)
+
+  
+const fruits = ["banane", "pomme", "orange"]
+for(i=0;i<fruits.length; i ++)
+console.log(`${i} = ${fruits[i]}`)
+
+
+fruits.forEach((fruit)=>{
+console.log(`fruit:${fruit}`)})
+
+fruits.forEach((fruit)=>{console.log(fruit)})
+
+for (i=0;i<fruits.length;i++)
+console.log(fruits[i])
+
+// while fruits
+
+
+
+//let cats = ['bill', 'jeff', 'pete', 'biggles', 'jasmin'];
+//for(let i= 0; i < cats.length; i++) {
+//cats[i] = cats[i].charAt(0).toUpperCase() + cats[i].slice(1))
+    
+
+
+//if i%2=0 alors true then c'est pair if not, sinon c'est impair
+//if (i%2=0?true"nombre pair":false"nombre impair"))
+
+
+//console.log(`$[i]`=)
+
+for(i=0 ; i<10 ;i++){
+   if (i%2 == 0 ) {
+    console.log(`${i} : nombre pair`);
+    } 
+    else {
+    console.log(`${i} : nombre impair`);
+    } }
+
+  
+  let i=4
+  if (i%2==0){
+    console.log(`${i} est un nombre pair`)}
+    else {
+        console.log(`${i} est un nombre impair`)
+    }
+
